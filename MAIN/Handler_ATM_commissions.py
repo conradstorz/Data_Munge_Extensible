d9971dc9b6941f953f5622fff1692f1b937575c0
Conrad Storz <conradstorz@gmail.com>
2024-08-01 10:07:36 -0400

    first working commit

--- MAIN/Handler_ATM_commissions.py
+++ MAIN/Handler_ATM_commissions.py
@@ -1,16 +1,17 @@
 """Handler for PAI report of locations earning a commission for a period.
 
-This code started as the code for 'Process_surcharge'
-
+data in csv is:
+"Location","WD Trxs","Surcharge WDs","Settlement","Group"(2 items; string: group name, float: commission rate)
 """
 
 
 import pandas as panda
-import json
 from loguru import logger
 from pathlib import Path
-from whenever import Instant
+from dataframe_functions import extract_date_from_filename
 from dataframe_functions import save_results_and_print
+from dataframe_functions import data_from_csv
+from dataframe_functions import dataframe_contains
 
 # standardized declaration for CFSIV_Data_Munge_Extensible project
 FILE_EXTENSION = ".csv"
@@ -45,216 +46,111 @@ declaration = Declaration()
 
 
 @logger.catch
-def handler_process(file_path: Path) -> bool:
-    # This is the standardized function call for the Data_Handler_Template
-    result = empty_df = panda.DataFrame()
+def handler_process(file_path: Path):
+    # This is the standardized functioncall for the Data_Handler_Template
     if not file_path.exists:
         logger.error(f'File to process does not exist.')
         return False
+
+    logger.info(f"Looking for date string in: {file_path.stem}")
+    filedate = extract_date_from_filename(file_path.stem)  # filename without extension
+    logger.debug(f"Found Date: {filedate}")       
+    output_file = Path(f'{ARCHIVE_DIRECTORY_NAME}{OUTPUT_FILE_EXTENSION}')
+    logger.debug(f'Output filename: {output_file}')
+    # launch the processing function
+    try:
+        result = process_commission_report(file_path, filedate)
+        # processing done, send result to printer
+    except Exception as e:
+        logger.error(f'Failure processing dataframe: {e}')
+        return False
     else:
-        # process file
-        output_file = Path(f'{ARCHIVE_DIRECTORY_NAME}{OUTPUT_FILE_EXTENSION}')
-        logger.debug(f'Output filename: {output_file}')        
-        try:
-            Input_df, INPUTDF_TOTAL_ROWS, column_details, terminal_details, LOCATION_TAG = process_monthly_surcharge_report(file_path, Instant.now())
-            # processing done, send result to printer
-        except Exception as e:
-            logger.error(f'Failure processing dataframe: {e}')
-            return False
+        if len(result) > 0:
+            save_results_and_print(output_file, result, file_path)
         else:
-            # dataframe needs to have additional columns calculated
-            new_df = calculate_additional_values(Input_df, terminal_details, column_details)            
-            if len(new_df) > 0:
-                logger.info(f'Send dataframe to be made into commission report')
-                frame = generate_multiple_report_dataframes(column_details, Input_df, INPUTDF_TOTAL_ROWS, LOCATION_TAG)
-            else:
-                logger.error(f'No data found to process')
-                return False
-            if len(frame) > 0:
-                logger.info(f'Send dataframe to printer')
-                for filename, df in frame:
-                    save_results_and_print(filename, df, "")  # empty string tells function not to move input file to history
-            else:
-                logger.error(f'No data frames found to print')
+            logger.error(f'No data found to process')
+            return False
     # all work complete
     return True
 
-
-
-# Custom function to serialize the dictionary excluding unserializable objects
-def custom_json_serializer(obj):
-    # usage example: pretty_dict = json.dumps(dict, default=custom_json_serializer, indent=4)
-    if isinstance(obj, (int, float, str, bool, list, tuple, dict, type(None))):
-        return obj
-    else:
-        return str(obj)
-    
-from customize_dataframe_for_excel import set_custom_excel_formatting
-
-def validate_value(value, min_value=0, max_value=float('inf')):
-    """Validate if the value is within the expected range and is not negative.
-
-    Args:
-        value (float): The value to validate.
-        min_value (float): The minimum acceptable value. Defaults to 0.
-        max_value (float): The maximum acceptable value. Defaults to infinity.
-
-    Returns:
-        bool: True if value is valid, False otherwise.
-    """
-    if value < min_value or value > max_value:
-        return False
-    return True
-
-
 @logger.catch
-def process_monthly_surcharge_report(input_file, RUNDATE):
-    """takes a 'csv' file and returns a dataframe
-    """
-    # pandas tags:
-    LOCATION_TAG = "Location"
-    DEVICE_NUMBER_TAG = "Device Number"
-
-    DAYS = 30  # most months are 30 days and report covers a month
-    # TODO not all reports are 30 days. Some are 90 days. Try to determine actual number of days.
-    OPERATING_LABOR = 25  # estimated labor per visit in dollars.
-    logger.info("Beginning process of monthly report.")
-    logger.info(f"File: {input_file}")
-
-    def moveLast2first(df):
-        cols = df.columns.tolist()
-        cols = cols[-1:] + cols[:-1]
-        return df[cols]
-
-    empty_df = panda.DataFrame() 
-    # load the data from filename provided
-    try:
-        Input_df = panda.read_csv(input_file)
-    except Exception as e:
-        logger.error(f'Problem using pandas: {e}')
-        return (empty_df, 0, "")
+def process_commission_report(in_f, RUNDATE):
+    """Scan file and compute sums for 2 columns"""
+    # import data
+    empty_df = panda.DataFrame()
+    df = data_from_csv(in_f)
     
-    INPUTDF_TOTAL_ROWS = len(Input_df)
+    expected_fields_list = ["Location","WD Trxs","Surcharge WDs","Settlement","Group"]
 
-    logger.info(f"csv file imported into dataframe with {INPUTDF_TOTAL_ROWS} rows.")
-    logger.debug(Input_df.columns)
+    actual_columns_found = dataframe_contains(df, expected_fields_list)
+    
+    if not (actual_columns_found == expected_fields_list):
+        logger.debug(f'Data was expected to contain: {expected_fields_list}\n but only these fileds found: {actual_columns_found}')
+        return empty_df
+    
+    logger.info(f'Data contained all expected fields.')
 
-    # TODO combine entries that reference the same terminal in different months.
-    #       ...Reports that cover more than 1 month have seperate lines for each monthly period.
-    Input_df = Input_df.groupby(
-        [Input_df[LOCATION_TAG], Input_df[DEVICE_NUMBER_TAG]], as_index=False
-    ).sum(numeric_only=True)
+    # tack on the date of this report extracted from the filename
+    df.at[len(df), "Location"] = f"Report ran: {RUNDATE}"
 
-    INPUTDF_TOTAL_ROWS = len(Input_df)
-
-    logger.info(
-        f"{INPUTDF_TOTAL_ROWS} rows remain after combining identical locations."
-    )
-
-    # slice the terminal numbers and write to temp storage
+    # Strip out undesirable characters from "Settlement" column
     try:
-        t = Input_df[DEVICE_NUMBER_TAG]
-        t.to_json("temp.json", indent=4)
-        # TODO use this to determine which new terminals are missing from value lookup
+        df["Settlement"] = df["Settlement"].replace("[\$,)]", "", regex=True)
+        df["Settlement"] = df["Settlement"].astype(float)
     except KeyError as e:
-        logger.error(f"Error {e}")
+        logger.error(f"KeyError in Settlement column: {e}")
+        return empty_df
 
-    with open(VALUE_FILE) as json_data:
-        terminal_details = json.load(json_data)
-    # this dictionary will contain information about individual terminals
-    # Pretty print and log the dictionary item
-    pretty_json = json.dumps(terminal_details, default=custom_json_serializer, indent=4)
-    logger.debug(f'Printer details report:\n{pretty_json}')
+    # Process "WD Trxs" column
+    try:
+        df.replace({"WD Trxs": {"[\$,)]": ""}}, regex=True, inplace=True)
+        df["WD Trxs"] = panda.to_numeric(df["WD Trxs"], errors="coerce")
+    except KeyError as e:
+        logger.error(f"KeyError in 'WD Trxs' column: {e}")
+        return empty_df
 
-    logger.info(f"Reading formatting file..")
-    with open(FORMATTING_FILE) as json_data:
-        column_details = json.load(json_data)
-    # this dictionary will contain information about formating output values.
-    # Pretty print and log the dictionary item
-    pretty_json = json.dumps(column_details, default=custom_json_serializer, indent=4)
-    logger.debug(f'Printer details report:\n{pretty_json}')
+    # Process "Surcharge WDs" column
+    try:
+        # df["Surcharge WDs"] = df["Surcharge WDs"].astype(int)  was throwing exception
+        df["Surcharge WDs"] = panda.to_numeric(df["Surcharge WDs"], errors='coerce').astype('Int64')
+    except KeyError as e:
+        logger.error(f"KeyError in 'Surcharge WDs' column: {e}")
+        return empty_df
 
-    return (Input_df, INPUTDF_TOTAL_ROWS, column_details, terminal_details, LOCATION_TAG)
+    # process the "group" column
+    # Define the regular expression pattern
+    find_monetary_value = r'^(.*?),?\s*([$]?\d+\.\d+)$'
+
+    # Extract the commission_rate from the remainder of the Group column
+    try:
+        df[['groupname', 'comm_rate']] = df['Group'].str.extract(find_monetary_value)
+    except KeyError as e:
+        logger.error(f"KeyError in 'Group' column: {e}")
+        return empty_df
+
+    # Convert commission_rate to float
+    try:
+        df['comm_rate'] = df['comm_rate'].str.replace('$', '').astype(float)    
+    except KeyError as e:
+        logger.error(f"KeyError in 'Surcharge WDs' column: {e}")
+        return empty_df
+    except ValueError as e:
+        logger.error(f'Error converting string to float: {e}')
+        df['comm_rate'] = 0.0
 
 
-""" these are the calculations used for dupont analysis
-Commission_due = df['SurWD Trxs'] * terminal_details[VF_KEY_Commissions]
-***This is a cheat sheet to values used in a dupont analysis
-Annual_Net_Income = (df['Business Total Income'] - Commission_due) * 12       # (annualized)
-Annual_WDs = df['SurWD Trxs'] * 12
-Annual_Gross_Surcharge = df['Total Surcharge'] * 12
-Period_Dispensed_Amount = df['Total Dispensed Amount']
-Average_Surcharge = Annual_Net_Income / Annual_WDs
-Surcharge_Percentage = Annual_Net_Income / Annual_Gross_Surcharge
-Average_Daily_Dispense = Period_Dispensed_Amount / DAYS
-Current_Assets = Average_Daily_Dispense * 14 # Float plus Vault
-Assets = FIXED_ASSETS + Current_Assets
-Asset_Turnover = Annual_Net_Income / Assets
-Earnings_BIT = Annual_Net_Income - OPERATING_EXPENSES
-Profit_Margin = Earnings_BIT / Annual_Net_Income
-R_O_I = Asset_Turnover * Profit_Margin
-"""
+    # calculate the commission due
+    try:
+        df['Commission_Due'] = df['comm_rate'] * df["Surcharge WDs"]    
+    except KeyError as e:
+        logger.error(f"KeyError in 'Surcharge WDs' column: {e}")
+        return empty_df
 
+    # sort the data so the totals are listed at the top of the report
+    df = df.sort_values("Settlement", ascending=False)
+    logger.debug(f'Dataframe finished: {df}')
 
-def calculate_additional_values(df, terminal_details, column_details):
-    # Constants
-    DAYS = 30
-    FIXED_ASSETS = 1000
-    OPERATING_EXPENSES = 50
-    OPERATING_LABOR = 25 
+    # work is finished. Drop unneeded columns from output
+    df = df.drop(["groupname","Group","Settlement", "Surcharge WDs"], axis=1)
 
-    VF_KEY_Commission_rate = "Comm Rate paid"
-
-    # These names must match the input dataframe columns
-    SURCHXACTS = "Surcharge WDs"
-    GROUPDETAILS = "Group"
-    LOCATION_TAG = "Location"
-
-    # These names are added to the original input dataframe
-    COMMTOBEPAID = "Comm_Due"
-
-    # Helper functions
-    def get_commission_due(row):
-        device_info = row.get(LOCATION_TAG, 'unknown')
-        group = row.get(GROUPDETAILS, 0)
-        _, commrate = group.split()
-        commrate = float(commrate)
-        # commrate = float(device_info.get(VF_KEY_Commission_rate, 0))
-        transactions = float(row.get(SURCHXACTS, 0))
-        return round(transactions * commrate, 2)
-
-    # Calculations
-    df[COMMTOBEPAID] = df.apply(get_commission_due, axis=1)
- 
     return df
-
-    # Example usage
-    # df = calculate_values(df, terminal_details, column_details)
-
-
-
-
-
-@logger.catch()
-def generate_report_dataframe(column_details, Input_df, INPUTDF_TOTAL_ROWS, LOCATION_TAG):
-    """send ATM terminal activity dataframe to file"""
-
-    with open(REPORT_DEFINITIONS_FILE) as json_data:
-        report_definitions = json.load(json_data)
-    # this dictionary will contain information about individual reports
-    # Pretty print and log the dictionary item
-    pretty_json = json.dumps(report_definitions, default=custom_json_serializer, indent=4)
-    logger.debug(f'Report Definitions File:{pretty_json}')        
-
-    logger.info(f'Generating report:')
-    # create a unique filename for each report
-    fn = f"_Outputfile{0}.xlsx"
-    # Creating an empty Dataframe with column names only
-
-    # fill those columns with data
-
-    logger.info(f'Dataframe with {len(frame[fn])} items created.')
-    # insert name of report into dataframe past the last row 
-    frame[fn].at[INPUTDF_TOTAL_ROWS + 1, LOCATION_TAG] = report
-logger.info(f'Finished creating {len(frame)} reports as dataframes.')
-return frame
