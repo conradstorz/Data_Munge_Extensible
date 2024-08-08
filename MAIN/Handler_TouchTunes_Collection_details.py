0707bb246197a17685d48744c489c32813e8aadb
Conrad Storz <conradstorz@gmail.com>
2024-08-07 21:23:39 -0400

    functional incremental improvements

--- MAIN/Handler_TouchTunes_Collection_details.py
+++ MAIN/Handler_TouchTunes_Collection_details.py
@@ -49,12 +49,19 @@ def handler_process(file_path: Path):
 
     logger.info(f"Looking for date string in: {file_path.stem}")
     filedate = extract_date_from_filename(file_path.stem)  # filename without extension
-    logger.debug(f"Found Date: {filedate}")       
-    output_file = Path(f'{ARCHIVE_DIRECTORY_NAME}{OUTPUT_FILE_EXTENSION}')
+    logger.debug(f"Found Date: {filedate}")
+    # this data has more needed details in the filename. example:   Collection Details (A79CD) May 17, 2024 (4).csv
+    # the jukebox ID is contained inside the first set of parenthesis and needs to be recovered and matched to the location name.
+    touchtunes_device = ID_inside_filename(file_path)
+
+    output_file = Path(f'{ARCHIVE_DIRECTORY_NAME}({touchtunes_device}){OUTPUT_FILE_EXTENSION}')
     logger.debug(f'Output filename: {output_file}')
     # launch the processing function
     try:
-        result = process_this_data(file_path, filedate)
+        result = aquire_this_data(file_path, filedate)
+        # now generate the output dataframe
+        output = process_df(result
+                            )
         # processing done, send result to printer
     except Exception as e:
         logger.error(f'Failure processing dataframe: {e}')
@@ -70,16 +77,49 @@ def handler_process(file_path: Path):
 
 
 @logger.catch
-def process_this_data(file_path: Path, date_str) -> bool:
+def aquire_this_data(file_path: Path, date_str) -> bool:
     # This is the customized procedures used to process this data. Should return a dataframe.
     empty_df = panda.DataFrame()
     
     print(f'{file_path} with embeded date string {date_str} readyness verified.')
+    # this csv file is organized with descriptions in column 0 and values in column 1
 
-    # problem with this csv file contains no headers
+    # Read the CSV file. the structure is   descript,value
+    #                                       descript,value
+    #                                       ...
 
-    df = load_csv_with_optional_headers(file_path, headers=['source', 'amount'])
+    df = panda.read_csv(file_path, index_col=0)  # do not place default index numbers
+
+    # Rotate the DataFrame 90 degrees to the right.
+    rotated_df = df.T
+    # now the descriptions are the headers
+                
+    print(f'{rotated_df}')
+
+    print(f'Items: {rotated_df.columns.to_list()}')
+    """['Multi-Credit Jukebox', 'Mobile', 'Karaoke', 'Photobooth', 'Unused credits', 'Cleared credits', 
+        'Total Revenue Breakdown', 'Bill', 'Coin', 'Subtotal (Bill + Coin)', 'Linked', 'CC/3rd Party', 'Mobile', 
+        'Total Revenue', '1 Credit Jukebox (music)', 'Multi-Credit Jukebox (music)', 'Mobile', 
+        'Karaoke service', 'Karaoke BGM', 'Karaoke plays', 'Photobooth print', 'Other fees', 
+        'Total Revenues', 'Total fees', 'Total to split', 'Location split', 'Operator split']
+    """
     
-    print(f'{df}')
     # all work complete
-    return empty_df
+    return rotated_df
+
+@logger.catch()
+def ID_inside_filename(fn):
+    # get the sub-string inside the parenthesis
+    parts = fn.split('(')
+    if len(parts) > 1:
+        subparts = parts[1].split(')')
+        if len(subparts) > 1:
+            return subparts[0]
+    return 'xxxxxx'
+
+
+
+@logger.catch()
+def process_df(df):
+
+    return df
