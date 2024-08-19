13f857c3e09711b7b18f733014f363e007dad461
Conrad Storz <conradstorz@gmail.com>
2024-08-19 15:39:37 -0400

    now working version of function based operation

--- MAIN/directory_watcher.py
+++ MAIN/directory_watcher.py
@@ -1,5 +1,9 @@
 import pickle
 from pathlib import Path
+from loguru import logger
+import time
+
+logger.catch()
 def get_first_new_file(directory_to_watch, pickle_file):
     """
     Checks the download directory, removes missing filenames from pickle,
@@ -37,9 +41,35 @@ def get_first_new_file(directory_to_watch, pickle_file):
         with open(pickle_file, 'wb') as f:
             pickle.dump(existing_files, f)
 
-        return new_file
+        return Path(new_file)
     else:
         return None
+
+
+@logger.catch()
+def monitor_download_directory(directory_to_watch, file_processor):
+
+    logger.info(f"Starting directory watcher on {directory_to_watch}")
+
+    try:
+        while True:
+            time.sleep(1)
+            logger.debug(f'Checking for new files in {directory_to_watch}')
+            new_file = get_first_new_file(directory_to_watch, './download_history_file.pkl')
+            logger.debug(f'found: {new_file}')
+            if new_file:
+                if Path(new_file).suffix not in ['.ini', '.tmp', '.png']:
+                    file_processor.process(directory_to_watch / new_file)
+                else:
+                    logger.debug(f'Ignoring file found: {new_file}')
+
+    except KeyboardInterrupt:
+        logger.info(f'Keyboard interrupt detected.')
+        logger.info("Directory watcher stopped")
+    logger.info(f'All processes ended.')
+    return True
+
+
 
 # Example usage
 new_file = get_first_new_file('./', './pickle_file.pkl')
@@ -47,3 +77,4 @@ if new_file:
     print(f"New file detected: {new_file}")
 else:
     print("No new files detected.")
+
