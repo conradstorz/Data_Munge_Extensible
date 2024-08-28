72793aa727f1d279ded900808a6566c56d4f829c
Conrad Storz <conradstorz@gmail.com>
2024-08-28 07:07:34 -0400

    updated template

--- MAIN/data_munge_handler_template.py
+++ MAIN/data_munge_handler_template.py
@@ -6,6 +6,8 @@ from dataframe_functions import save_results_and_print
 from dataframe_functions import data_from_csv
 from dataframe_functions import dataframe_contains
 
+SYSTEM_PRINTER_NAME = "Canon TR8500 series"  # SumatrPDF needs the output printer name
+
 # standardized declaration for CFSIV_Data_Munge_Extensible project
 INPUT_DATA_FILE_EXTENSION = ".csv"
 OUTPUT_FILE_EXTENSION = ".xlsx"  # if this handler will output a different file type
@@ -51,39 +53,46 @@ class Declaration:
 
 @logger.catch
 def handler_process(file_path: Path):
-    # This is the standardized functioncall for the Data_Handler_Template
+    # This is the standardized functioncall for the Data_Handler_Template    
+    logger.info(f'Handler launched on file {file_path}')
+
     if not file_path.exists:
-        logger.error(f"File to process does not exist.")
+        logger.error(f"File '{file_path}' to process does not exist.")
         return False
 
-    logger.info(f"Looking for date string in: {file_path.stem}")
-    filedate = extract_date_from_filename(file_path.stem)  # filename without extension
-    logger.debug(f"Found Date: {filedate}")
-    output_file = Path(f"{ARCHIVE_DIRECTORY_NAME}{OUTPUT_FILE_EXTENSION}")
+    logger.debug(f"Looking for date string in: {file_path.stem}")
+    filedates_list = extract_dates(file_path.stem)  # filename without extension
+    logger.debug(f"Found Date: {filedates_list}")
+
+    
+
+    output_file = Path(
+        f"{ARCHIVE_DIRECTORY_NAME}{OUTPUT_FILE_EXTENSION}"
+    )
     logger.debug(f"Output filename: {output_file}")
+
     # launch the processing function
     try:
-        result = process_this_data(file_path, filedate)
-        # processing done, send result to printer
+        logger.debug(f'Starting data aquisition.')
+        raw_dataframe = aquire_data(file_path, filedates_list)
     except Exception as e:
         logger.error(f"Failure processing dataframe: {e}")
         return False
-    else:
-        if len(result) > 0:
-            save_results_and_print(output_file, result, file_path)
-        else:
-            logger.error(f"No data found to process")
-            return False
-    # all work complete
+    if len(raw_dataframe) < 1:
+        logger.error(f"No data found to process")
+        return False
+
+    process_this_data(raw_dataframe, filedates_list, output_file)
     return True
 
-
 @logger.catch
-def process_this_data(file_path: Path, date_str) -> bool:
+def process_this_data(raw_dataframe, date_str, output_file) -> bool:
     # This is the customized procedures used to process this data. Should return a dataframe.
     empty_df = panda.DataFrame()
+
     # *** place custom code here ***
-    print(f"{file_path} with embeded date string {date_str} readyness verified.")
+    
+    print(f"{output_file} with embeded date string {date_str} readyness verified.")
 
     # all work complete
     return empty_df
