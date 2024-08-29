419c6c324961cd7a3335a97d89d85a18240c6aa0
Conrad Storz <conradstorz@gmail.com>
2024-08-29 19:16:46 -0400

    fixed imports

--- MAIN/Handler_TouchTunes_Collection_details.py
+++ MAIN/Handler_TouchTunes_Collection_details.py
@@ -3,9 +3,7 @@ from loguru import logger
 from pathlib import Path
 
 from generic_munge_functions import extract_dates
-from generic_dataframe_functions import apply_formatting_and_save
-from generic_dataframe_functions import convert_xlsx_2_pdf
-from generic_dataframe_functions import print_pdf_using_os_subprocess
+from generic_excel_functions import apply_formatting_and_save
 
 from TouchTunes_Jukebox_Details import (
     jukebox_data_for_ID,
