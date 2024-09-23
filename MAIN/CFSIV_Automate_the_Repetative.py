39d673ccb185de2d1147226a55a74105e6e74c4d
Conrad Storz <conradstorz@gmail.com>
2024-09-23 07:52:25 -0400

    comments cleanup

--- MAIN/CFSIV_Automate_the_Repetative.py
+++ MAIN/CFSIV_Automate_the_Repetative.py
@@ -11,6 +11,7 @@ swaps these fields and removes extraneous words like 'ckcd', 'ach', 'pos', etc..
 I have worked with ChatGPT to create a framework for these scripts that will monitor my downloads directory and launch
 the proper script for processing the data that is incoming.
 
+As of 9/22/2024 this framework has handlers for 11 different data sources.
 """
 
 from file_processor_and_scripts_manager import ScriptManager, FileProcessor
@@ -22,38 +23,44 @@ from dotenv import dotenv_values
 import sys
 
 # setup logging
-# Remove the default handler
-logger.remove()
-# Add a handler for the console that logs messages at the INFO level
-# Add the console handler with colorization enabled
+
+logger.remove()  # Remove the default handler
+
+# Add a handler for the console that logs messages at the INFO level with colorization enabled
 logger.add(sys.stdout, level='INFO', colorize=True, format="<green>{time}</green> <level>{message}</level>")
-# setup log rotation
+
+# Add a handler for permanent storage with automatic rotation
 logger.add("LOGS/file_processing_{time:YYYY-MM-DD}.log", rotation="00:00", retention="9 days")
+
 logger.info(f'Program Start: {__name__}')
 
 # load data processing functions for various data types
 SCRIPTS_DIRECTORY = Path.cwd() / "MAIN"
 scripts_manager_instance = ScriptManager(SCRIPTS_DIRECTORY)
 scripts_manager_instance.load_scripts()
+
+# Each script should define how to identify known data and what to do with that data
 file_processor_instance = FileProcessor(scripts_manager_instance)
 
 # establish where to look for incoming data
 DIRECTORY_TO_WATCH = Path("D:/Users/Conrad/Downloads/")
 
-# load email secrets
+# load secrets from the running system to ensure they are not hard coded
 SECRETS_DIRECTORY = SCRIPTS_DIRECTORY / Path(".env")
-secrets = dotenv_values(SECRETS_DIRECTORY)
+SECRETS = dotenv_values(SECRETS_DIRECTORY)  # load secrets from '.env' file 
 IMAP_SERVER = "imap.gmail.com"
 
-# initiate class  (has optional flag 'mark_as_read' that defaults to False)
+# initiate the email watcher class  (has optional flag 'mark_as_read' that defaults to False)
 try:
-    email_fetcher = EmailFetcher(IMAP_SERVER, secrets["EMAIL_USER"], secrets["EMAIL_PASSWORD"], interval=180, dld=DIRECTORY_TO_WATCH)
+    email_fetcher = EmailFetcher(IMAP_SERVER, SECRETS["EMAIL_USER"], SECRETS["EMAIL_PASSWORD"], interval=180, dld=DIRECTORY_TO_WATCH)
 except KeyError as e:
     logger.error(f'Could not initialize email fetcher. KeyError: {str(e)}')
 
 # start fetcher
 email_fetcher.start()
+# fetcher will download each email and it's attachments as it arrives
 
+# begin monitoring directory where data gets downloaded
 # This function will run until Keyboard Interrupt is detected
 monitor_download_directory(DIRECTORY_TO_WATCH, file_processor_instance, delay=2)
 
